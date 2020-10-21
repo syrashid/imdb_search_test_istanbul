bb73f3e8e4303512c16863bd532a9c8c4bfcaa2a
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-21 22:47:20 +0300

    FEAT: Finished lecture practice

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      @movies = Movie.search_by_title_and_syllabus(params[:query])
+    else
+      @movies = Movie.all
+    end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,15 @@
 class Movie < ApplicationRecord
+  searchkick
+
   belongs_to :director
+  # include PgSearch::Model
+  # multisearchable against: [:title, :syllabus]
+  # pg_search_scope :search_by_title_and_syllabus,
+  #   against: [ :title, :syllabus ],
+  #   associated_against: {
+  #     director: [ :first_name, :last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
